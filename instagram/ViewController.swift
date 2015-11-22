9be4701ef4a3cb569e2e006cfb34c6ec022f18fc
Stacey Gennoy <sgennoy@gmail.com>
2015-11-22 14:58:50 -0800

    Initial Commit

---
+++ instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Stacey Gennoy on 11/22/15.
+//  Copyright © 2015 Stacey Gennoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
